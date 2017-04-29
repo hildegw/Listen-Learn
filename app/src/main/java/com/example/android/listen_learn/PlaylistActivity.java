fe56018c5a184411aed09fbf4b160ae301f9db5a
hildegw <hildegw@snafu.de>
2017-04-29 12:04:09 -0500

    implemented all Basics

--- app/src/main/java/com/example/android/listen_learn/PlaylistActivity.java
+++ app/src/main/java/com/example/android/listen_learn/PlaylistActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 //This activity shows a user's playlist
@@ -16,13 +18,21 @@ public class PlaylistActivity extends AppCompatActivity {
         setContentView(R.layout.playlist);
 
         //todo: show user specific list of titles, stored locally in Shared Preferences
-        //todo: set up list view
-        String playlist1 = "A Dream within a Dream \nby Edgar Allen Poe";      //placeholder
-        TextView play1Text = (TextView) findViewById(R.id.play1);
-        play1Text.setText(playlist1);
+        //todo: set up list view, fetch items from Shared Preferences
+        String[] items = {"A Dream within a Dream \nby Edgar Allen Poe",            //placeholder
+                           "The Blind Boy\nby Colley Cibber",
+                            "Stand and sing\nJackie and Richard talk about national anthems",
+                            "The Looking Glass\nby Anton Chekhov",
+                            "The Cripple\nby Guy de Maupassant",
+                            "The Night Moth With a Crooked Feeler\nby Clara Dillingham Pierson\n"};
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, items);
+        ListView listView = (ListView) findViewById(R.id.play1);
+        listView.setAdapter(itemsAdapter);
 
         //todo: user can delete titles from playlist, i.e. edit Shared Preferences
         //todo: user can change order of titles in playlist, e.g. via clicking on up/down arrows
+        //todo: write own list item object including up/down/minus buttons, with own Adapter
 
         //Set click listeners and intents to go to other Activities
         View listenView = (View) findViewById(R.id.listen);
@@ -33,26 +43,11 @@ public class PlaylistActivity extends AppCompatActivity {
                 startActivity(listenIntent);
             }
         });
-        View popularView = (View) findViewById(R.id.popular);
-        popularView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent popularIntent = new Intent(PlaylistActivity.this, PopularActivity.class);
-                startActivity(popularIntent);
-            }
-        });
-        View browseView = (View) findViewById(R.id.browse);
-        browseView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent listenIntent = new Intent(PlaylistActivity.this, BrowseActivity.class);
-                startActivity(listenIntent);
-            }
-        });
 
         //todo explain Activity purpose
         String purpose = "The playlist is user specific and stored locally via Shared Preferences. " +
-                "Titles are presented as ListView, and can be deleted, or moved up and down (use Click Listeners). " +
+                "Titles are presented as ListView, and can be deleted, or moved up and down (use Click Listeners, " +
+                "write own Adapter Class to add up, down, minus buttons). " +
                 "Because this is a potentially long list, no cross-navigation to Browse and Popular is possible.";
         TextView listenPurpose = (TextView) findViewById(R.id.purpose);
         listenPurpose.setText(purpose);
