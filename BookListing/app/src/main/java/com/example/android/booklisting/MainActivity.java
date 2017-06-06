4e3c4cd4698a8c6ebf1dc4c7b36697b2c19dd410
hildegw <hildegw@snafu.de>
2017-06-06 10:30:05 -0500

    reading out search string

--- BookListing/app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ BookListing/app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.android.booklisting;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.method.KeyListener;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private KeyListener editTextKeyListener;
+    private EditText editTextSearchEntry;
 
 
     @Override
@@ -15,17 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Set keyListener for EditText field
-        EditText editTextSearchEntry = (EditText) findViewById(R.id.search_entry);
+        /*/Set keyListener for EditText field
         editTextSearchEntry.setKeyListener(editTextKeyListener);
-
         editTextKeyListener = editTextSearchEntry.getKeyListener();
-
         //Disable text entry
         editTextSearchEntry.setKeyListener(null);
-
-        //Read out EditText field, i.e. search string
-        String searchString = editTextSearchEntry.getText().toString().toLowerCase();
+*/
+        //Identify EditText field and Search button
+        editTextSearchEntry = (EditText) findViewById(R.id.search_entry);
+        Button searchButton = (Button) findViewById(R.id.search_button);
+        //When search button is clicked, read out search string
+        searchButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {String searchString = editTextSearchEntry.getText().toString().toLowerCase();}
+                });
 
     }
 }
